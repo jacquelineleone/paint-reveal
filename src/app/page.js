b57a13be3b2aa21124c706be9d93129c7c9d98d9
Jacqueline <jacqueline.leone02@gmail.com>
2024-08-21 17:53:20 -0300

    add: text component

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
+import Text from "@/components/text";
+
 export default function Home() {
   return (
-    <main className="flex w-full h-screen items-center justify-center"></main>
+    <main className="flex w-full h-screen items-center justify-center">
+      <Text />
+    </main>
   );
 }
---
+++ src/components/text/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Text() {
+  return (
+    <div className="absolute flex flex-col text-[5.5vw] uppercase w-[80vw] items-start leading-tight">
+      <p>Jacqueline Leone</p>
+      <p className="self-end">Front End Developer</p>
+      <p>Based in Buenos Aires</p>
+      <p className="self-end">Portfolio in progress</p>
+    </div>
+  );
+}

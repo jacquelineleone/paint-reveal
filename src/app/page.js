205a6a5b366155dfa1686e5490b0110d9b9869c0
Jacqueline <jacqueline.leone02@gmail.com>
2024-08-22 11:00:38 -0300

    add: render scene

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Scene from "@/components/scene";
 import Text from "@/components/text";
 
 export default function Home() {
   return (
     <main className="flex w-full h-screen items-center justify-center">
       <Text />
+      <Scene />
     </main>
   );
 }
